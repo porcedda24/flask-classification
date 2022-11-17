cb94d3577958dc4b4f80e66e72511951b84a0b4a
porcedda24 <95358876+porcedda24@users.noreply.github.com>
2022-11-17 21:07:43 +0100

    Update classifications_id.py

--- app/routes/classifications_id.py
+++ app/routes/classifications_id.py
@@ -11,8 +11,12 @@ config = Configuration()
 def classifications_id(job_id):
     """Returns the status and the result of the job identified
     by the id specified in the path."""
-    task = None  # TODO connect to the database and get the task by the id
-
+      # TODO connect to the database and get the task by the id
+    redis_url = config.REDIS_URL
+    redis_connection = redis.from_url(redis_url)
+    with Connection(redis_connection):
+        q = Queue(name=config.QUEUE)
+        task = q.fetch_job(job_id)
     response = {
         'task_status': task.get_status(),
         'data': task.result,
