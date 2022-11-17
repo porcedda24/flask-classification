fc50b9da66a56e97a620fb78f31203d6b45b8483
porcedda24 <95358876+porcedda24@users.noreply.github.com>
2022-11-17 21:08:10 +0100

    Update info.py

--- app/routes/info.py
+++ app/routes/info.py
@@ -1,8 +1,13 @@
 from app import app
-
+from app.utils.list_images import list_images
+from config import Configuration
+conf = Configuration()
 
 @app.route('/info', methods=['GET'])
 def info():
     """Returns a dictionary with the list of models and
     the list of available image files."""
-    pass
+    data = dict()
+    data['models'] = conf.models
+    data['images'] = list_images()
+    return data
